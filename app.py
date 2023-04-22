87d73ac3013a659df084ad7229d17f6efbed223c
thisissingh19@gmail.com <thisissingh19@gmail.com>
2023-04-22 17:07:00 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
+#header section
+st.subheader("Hello World")
+st.title("Streamlit")
+st.write("This website is designed using streamlit")
+st.write("[Learn More about streamlit on] (https://docs.streamlit.io/)")
