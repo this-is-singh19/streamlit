8653bad80b482ed8ed1a8b71022e98a8f56625cd
thisissingh19@gmail.com <thisissingh19@gmail.com>
2023-04-22 17:29:54 +0530

    Added json animation section

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
+
 st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
 #header section
 with st.container():
@@ -6,6 +9,17 @@ with st.container():
 	st.title("Streamlit")
 	st.write("This website is designed using streamlit")
 	st.write("[Learn More about streamlit on] (https://docs.streamlit.io/)")
+
+#adding animation 
+
+def loadanime(url):
+	r = requests.get(url)
+	if r.status_code != 200:
+		return None
+	return r.json()
+
+#load animation
+questionani = loadanime("https://assets1.lottiefiles.com/temp/lf20_9gY9Yf.json")
 
 #What streamlit does
 with st.container():
@@ -27,3 +41,5 @@ with st.container():
 			enabling faster experimentation and collaboration in data science projects.
 			"""
 		)
+	with right_column:
+		st_lottie(questionani,height=300,key="question") 
